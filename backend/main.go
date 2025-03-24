444101cd896051aa57611858c73fbc3386832fbf
gildemeber-santos <gildemeber.santos@gmail.com>
2025-03-23 22:05:23 -0300

    Adicionar suporte ao carregamento de variáveis de ambiente com godotenv e atualizar o manipulador de requisições para retornar JSON

--- backend/main.go
+++ backend/main.go
@@ -3,14 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("frontend/build"))
+	godotenv.Load()
+
+	fs := http.FileServer(http.Dir(os.Getenv("PATH_REACT")))
 	http.Handle("/", fs)
 
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, world!"))
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "Hello World"}`))
 	})
 
 	fmt.Println("Server is running on port 8080 ...")
