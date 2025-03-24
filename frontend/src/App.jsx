1ca7ac7be328ce1eedf7195d8bb065b0abde9bf2
gildemeber-santos <gildemeber.santos@gmail.com>
2025-03-24 20:53:08 -0300

    Adicionar componente WeddingPhoto ao App e ajustar filtro de saturação no CSS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import WeddingDate from './components/WeddingDate';
 import WeddingList from './components/WeddingList';
 import WeddingLocation from './components/WeddingLocation';
 import WeddingModal from './components/WeddingModal';
+import WeddingPhoto from './components/WeddingPhoto';
 import useSettings from './hooks/useSettings';
 import useWeddingList from './hooks/useWeddingList';
 import backgroundImage from "./textura.png";
@@ -40,6 +41,7 @@ function App() {
         title="Localização do Casamento"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d260.1550136434166!2d-38.45137562854816!3d-3.798811071446984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7c745af2d77f751%3A0x33210c6d90df141!2sR.%20Sabiaguaba%2C%201270%20-%20Edson%20Queiroz%2C%20Fortaleza%20-%20CE%2C%2060835-750!5e1!3m2!1spt-BR!2sbr!4v1742851282611!5m2!1spt-BR!2sbr"
       />
+      <WeddingPhoto />
       <Contacts
         whatsappGroom={whatsappGroom}
         whatsappBride={whatsappBride}
