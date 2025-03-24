d9c6ad89ab51e61b3958d877c257140b8accdadd
gildemeber-santos <gildemeber.santos@gmail.com>
2025-03-24 20:52:38 -0300

    Remover classe de fundo do cabeçalho para simplificar o estilo

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header({ title, description }) {
   return (
-    <div className="bg-light mb-4 p-4">
+    <div className="mb-4 p-4">
       <h1 className="my-4 wedding-text text-center font-cursive">{title}</h1>
       <p className="body-text text-center font-roboto font-size-1-2em">{description}</p>
     </div>
