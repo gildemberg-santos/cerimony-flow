07da20fd26b5c053c52426ecc281a9bfcc11e675
gildemeber-santos <gildemeber.santos@gmail.com>
2025-03-25 17:07:27 -0300

    Adicionar SubTitleCustom ao componente WeddingPhoto e ajustar estrutura

--- frontend/src/components/WeddingPhoto/index.jsx
+++ frontend/src/components/WeddingPhoto/index.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Card, Col, Row } from 'react-bootstrap';
 import usePhotos from '../../hooks/usePhotos';
+import SubTitleCustom from '../SubTitleCustom';
 
-function WeddingPhoto() {
+function WeddingPhoto({title}) {
   const photos = usePhotos();
 
   return (
-    <Container>
-      <Row>
+    <div className="mb-4 p-4">
+      <SubTitleCustom>{title}</SubTitleCustom>
+      <Row className="justify-content-center">
         {photos && photos.map((photo, index) => (
-          <Col md={4} key={index}>
-            <Card className="mb-4 shadow-sm">
+          <Col key={index} xs={12} sm={6} md={4} lg={3} className="mb-4">
+            <Card className="shadow-sm border-0">
               <Card.Img variant="top" src={photo} alt={`Foto ${index + 1}`} />
             </Card>
           </Col>
         ))}
       </Row>
-    </Container>
+    </div>
   );
 }
 
