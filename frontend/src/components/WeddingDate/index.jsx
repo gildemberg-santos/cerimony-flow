28a1d71082986d59fa8ba2a20be515d517d29922
gildemeber-santos <gildemeber.santos@gmail.com>
2025-03-25 17:06:52 -0300

    Adicionar SubTitleCustom ao componente WeddingDate e ajustar estilos

--- frontend/src/components/WeddingDate/index.jsx
+++ frontend/src/components/WeddingDate/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import useCountdown from '../../hooks/useCountdown';
+import SubTitleCustom from '../SubTitleCustom';
 
 function formatDate(dateString) {
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
@@ -19,18 +20,18 @@ function WeddingDate({ title, date }) {
     timerComponents.push(
       <div key={interval} className="border border-light p-4 d-inline-block mx-2">
         <div className="d-flex flex-column align-items-center">
-        <span className="timer-value">{timeLeft[interval]}</span>
-        <span className="timer-label">{interval}</span>
+          <span className="timer-value">{timeLeft[interval]}</span>
+          <span className="timer-label">{interval}</span>
         </div>
       </div>
     );
   });
 
   return (
-    <div className="bg-olive p-4">
-      <h2 className="wedding-text text-center font-cursive text-light">{title}</h2>
-      <p className="body-text text-center font-roboto font-size-1-2em text-light">{formatDate(date)}</p>
-      <div className="body-text text-center font-roboto font-size-1-2em text-light">
+    <div className="bg-primary p-4 pb-5">
+      <SubTitleCustom className="text-white">{title}</SubTitleCustom>
+      <p className="text-center font-roboto text-light fs-5">{formatDate(date)}</p>
+      <div className="text-center font-roboto text-light">
         {timerComponents.length ? timerComponents : <span>Já chegou!</span>}
       </div>
     </div>
