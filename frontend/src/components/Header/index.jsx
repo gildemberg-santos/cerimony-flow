9b073cdf60c8478ccb982d08be62487ced987316
gildemeber-santos <gildemeber.santos@gmail.com>
2025-03-25 17:06:31 -0300

    Adicionar componente Header com brasão e imagem de fundo, substituindo título por TitleCustom e ajustando estilos

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import brasao from '../../brasao.png';
+import background from '../../cerimonia.jpeg';
+import TitleCustom from '../TitleCustom';
+import './styles.css';
 
 function Header({ title, description }) {
   return (
     <div className="mb-4 p-4">
-      <h1 className="my-4 wedding-text text-center font-cursive">{title}</h1>
-      <p className="body-text text-center font-roboto font-size-1-2em">{description}</p>
+      <div className="crest text-center mb-2">
+        <img src={brasao} alt="Brasão" width={200} height={200} />
+      </div>
+      <TitleCustom>{title}</TitleCustom>
+      <div className="image-container mb-4">
+        <img src={background} alt="Cerimônia" className="img-fluid w-100 cropped-image filter-image" />
+      </div>
+      <p className="text-center font-roboto font-size-1-2em">{description}</p>
     </div>
   );
 }
