83a0ba3fad36d6017a701526738f61c6a3b881e0
gildemeber-santos <gildemeber.santos@gmail.com>
2025-03-25 17:07:00 -0300

    Substituir título por SubTitleCustom e atualizar botão para ButtonCustom no componente WeddingList

--- frontend/src/components/WeddingList/index.jsx
+++ frontend/src/components/WeddingList/index.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Button, Card, Col, Row } from 'react-bootstrap';
+import { Card, Col, Row } from 'react-bootstrap';
+import ButtonCustom from '../ButtonCustom';
+import SubTitleCustom from '../SubTitleCustom';
 
-function WeddingList({ weddingList, handleShowModal }) {
+function WeddingList({ title, weddingList, handleShowModal }) {
   return (
     <div className="mb-4 p-4">
-      <h2 className="my-4 wedding-text text-center font-cursive">Lista de Casamento</h2>
+      <SubTitleCustom>{title}</SubTitleCustom>
       <Row className="justify-content-center">
         {weddingList.map((item, index) => (
           <Col key={index} xs={12} sm={6} md={4} lg={3} className="mb-4">
             <Card className="shadow-sm border-0">
               <Card.Img variant="top" src={item.picture} alt={item.title} className="p-4 rounded" />
               <Card.Body className="text-center">
-                <Card.Title className="body-text font-roboto font-size-1-1em text-olive">{item.title}</Card.Title>
-                <Button variant="outline-olive body-text" onClick={() => handleShowModal(item)}>Detalhes</Button>
-                <p className={`mt-2 body-text ${item.available ? 'text-success' : 'text-danger'} font-size-0-9em`}>
+                <Card.Title className="font-roboto font-size-1-1em text-olive">{item.title}</Card.Title>
+                <ButtonCustom onClick={() => handleShowModal(item)}>Detalhes</ButtonCustom>
+                <p className={`mt-2 ${item.available ? 'text-success' : 'text-danger'} fs-6`}>
                   {item.available ? "Disponível" : "Indisponível"}
                 </p>
               </Card.Body>
