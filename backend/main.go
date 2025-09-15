1a69cd3de5fc2cf5f0ec555ac1e51b79ceb1b688
gildemberg-santos <gildemberg.santos@gmail.com>
2025-09-14 21:14:43 -0300

    Adicionar arquivos JSON para lista de casamento e fotos

--- backend/main.go
+++ backend/main.go
@@ -57,213 +57,42 @@ var settings = Settings{
 	CellPhoneBride: whatsappBride,
 }
 
-var weddingList = []Wedding{
-	{
-		ID:          1,
-		Title:       "Micro-ondas",
-		Description: "Micro-ondas Electrolux Prata com Painel Integrado 31l Mi41s 220v",
-		Picture:     "https://m.media-amazon.com/images/I/51jWG1vyGyL._AC_SL1500_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B076XC1QX7",
-		Available:   true,
-	},
-	{
-		ID:          2,
-		Title:       "Cafeteira Eletrica",
-		Description: "Cafeteira Eletrica Electrolux Programavel Com Timer 30 Xicaras Experience ECM 25-220V",
-		Picture:     "https://m.media-amazon.com/images/I/51OyvPACDML._AC_SL1000_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B09SVPS7S8",
-		Available:   true,
-	},
-	{
-		ID:          3,
-		Title:       "Torradeira",
-		Description: "Torradeira tostador Electrolux 8 niveis de tostagem função descongelar reaquecer cancelar bandeja coletora migalhas botões luminosos ETS25 inox 220v",
-		Picture:     "https://m.media-amazon.com/images/I/51pcpIXaj0L._AC_SL1000_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B09CBT3BY5",
-		Available:   true,
-	},
-	{
-		ID:          4,
-		Title:       "Sanduicheira",
-		Description: "Sanduicheira grill chapa ondulada elétrico Electrolux antiaderente inox ESG20 220v",
-		Picture:     "https://m.media-amazon.com/images/I/511Bqbpr12L._AC_SL1000_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B0CCBY3YMC",
-		Available:   false,
-	},
-	{
-		ID:          5,
-		Title:       "Xícaras",
-		Description: "Conjunto De 6 Xícaras Grandes 220 Ml Com Pires Ryo Pink Sand - Oxford",
-		Picture:     "https://m.media-amazon.com/images/I/41MufvzLJ0L._AC_SL1000_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B07T39LN9D",
-		Available:   true,
-	},
-	{
-		ID:          6,
-		Title:       "Taças",
-		Description: "Jogo de 6 Taças Vinho Tinto Xtra Cristal 460ml A24cm",
-		Picture:     "https://m.media-amazon.com/images/I/31EkEkILE5L._AC_SL1000_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B0B5LMNG3R",
-		Available:   true,
-	},
-	{
-		ID:          7,
-		Title:       "Airfryer",
-		Description: "Fritadeira Elétrica Grand Airfryer 4L Electrolux EAF30 Grafite",
-		Picture:     "https://m.media-amazon.com/images/I/51qMgG4EBUL._AC_SL1000_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B09NRYJ67D",
-		Available:   true,
-	},
-	{
-		ID:          8,
-		Title:       "Assadeiras",
-		Description: "Jogo de assadeiras Marinex Opaline com três peças",
-		Picture:     "https://m.media-amazon.com/images/I/51rrb7dMmGL._AC_SL1500_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B087699KMS",
-		Available:   true,
-	},
-	{
-		ID:          9,
-		Title:       "Travessa",
-		Description: "Conjunto 3 Travessa Melamina Branca Vasilhas Retangular Pequena Media e Grande de Mesa Posta Jogo de Tigelas Resistentes de Servir Arroz Macarrão e Salada. Ideal para Sobremesa Petiscos Frutas",
-		Picture:     "https://m.media-amazon.com/images/I/41BQtWzApnL._AC_SL1000_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B0D8NLYL2T",
-		Available:   true,
-	},
-	{
-		ID:          10,
-		Title:       "Assadeiras",
-		Description: "Conjunto de Assadeiras 5 Peças, Assadeira, Assadeira Antiaderente, Forma, Assadeira, Assadeira Antiaderente, Forma, Forma Assadeira, Formas e Assadeiras, Assadeiras, Forma Cupcake",
-		Picture:     "https://m.media-amazon.com/images/I/7158Fu0Ey6L._AC_SL1500_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B0DQD1GHSP",
-		Available:   true,
-	},
-	{
-		ID:          11,
-		Title:       "Potes",
-		Description: "Kit 10 Potes Herméticos C/Trava de Segurança Vedação de Borracha Porta Mantimentos Empilháveis + Etiquetas Lousa para Identificação",
-		Picture:     "https://m.media-amazon.com/images/I/717+18FOhgL._AC_SL1200_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B0CZYK2VMG",
-		Available:   true,
-	},
-	{
-		ID:          12,
-		Title:       "Sapateira",
-		Description: "Sapateira Articulada Safira 2 Portas Basculante Mell (Branco)",
-		Picture:     "https://m.media-amazon.com/images/I/31picHZt-QL._AC_SL1000_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B0CC673K8D",
-		Available:   true,
-	},
-	{
-		ID:          13,
-		Title:       "Travesseiro",
-		Description: "Kit Travesseiro 04 Peças Fiber Team Antialérgico com Fibra Siliconizada - Ortobom",
-		Picture:     "https://m.media-amazon.com/images/I/71fxsaGvfYL._AC_SL1500_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B08TMDFMN1",
-		Available:   true,
-	},
-	{
-		ID:          14,
-		Title:       "Colcha",
-		Description: "Colcha de Cama Casal 3 peças Luxo Algodão Cinza Menina Dupla Face Masculino Feminino (Bege Rosada, Casal)",
-		Picture:     "https://m.media-amazon.com/images/I/71zTINHgKqL._AC_SL1500_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B0FF9CCB3X",
-		Available:   true,
-	},
-	{
-		ID:          15,
-		Title:       "Colcha",
-		Description: "Kit Colcha Balle Para Cama King 400 Fios Dupla Face 3 Peças Cores Lisas Tecido Piquet Toque Mácio Acompanha Cobre Leito e 2 Porta Travesseiros (Tiffany, Casal King (260x280cm))",
-		Picture:     "https://m.media-amazon.com/images/I/613JI8rSTML._AC_SL1000_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B0DPGPGY5B",
-		Available:   true,
-	},
-	{
-		ID:          16,
-		Title:       "Colchão",
-		Description: "Colchão Casal de espuma D28 Emma Basics 17cm – com tecnologia alemã, 100 noites de teste e 5 anos de garantia",
-		Picture:     "https://m.media-amazon.com/images/I/7102jT-oGNL._AC_SL1500_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B0B8ZF9DR3",
-		Available:   true,
-	},
-	{
-		ID:          17,
-		Title:       "Kit Banheiro",
-		Description: "Kit Banheiro Lavabo Conjunto Acessórios lixeira escova 6 Peças Bambu Luxo Preto/Branco",
-		Picture:     "https://down-br.img.susercontent.com/file/br-11134207-7r98o-mcjiyijarzua36.webp",
-		Link:        "https://shopee.com.br/product/397571861/23393873729",
-		Available:   true,
-	},
-	{
-		ID:          18,
-		Title:       "Aspirador",
-		Description: "WAP Aspirador de Pó Robô ROBOT W90 3 em 1, Automático, 250ml, Sistema Antiqueda e Rodas Emborrachadas, 30W 3,6VDC Bivolt",
-		Picture:     "https://m.media-amazon.com/images/I/716GA5MQQkL._AC_SL1500_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B0B9PSBNYL",
-		Available:   true,
-	},
-	{
-		ID:          19,
-		Title:       "Batedeira",
-		Description: "Batedeira Planetária, Mondial, Preto/Inox, 700W, 110V - BP-01P-B",
-		Picture:     "https://m.media-amazon.com/images/I/811erab5-PL._AC_SL1500_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B07WDCZWFW",
-		Available:   true,
-	},
-	{
-		ID:          20,
-		Title:       "Panela pressão elétrica",
-		Description: "Panela pressão elétrica Electrolux digital capacidade 6L silenciosa segura 10 travas segurança 15 receitas pré-programadas 3 níveis pressão PCC20 inox preto 127v por Rita Lobo",
-		Picture:     "https://m.media-amazon.com/images/I/51zXHW5nVjL._AC_SL1000_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B076HYKFL7",
-		Available:   true,
-	},
-	{
-		ID:          21,
-		Title:       "Panificadora",
-		Description: "Panificadora Automática Master Bread, Mondial, Preto, 700W, 220V - NPF-53",
-		Picture:     "https://m.media-amazon.com/images/I/81G6rXDd9iL._AC_SL1500_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B07L8X97SR",
-		Available:   true,
-	},
-	{
-		ID:          22,
-		Title:       "Toalhas de Banho",
-		Description: "Buddemeyer Jogo de Toalhas Amanhecer Banho Branco 5 peças",
-		Picture:     "https://m.media-amazon.com/images/I/511VoVajAVL._AC_SL1000_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B0DWY1CHR4",
-		Available:   true,
-	},
-	{
-		ID:          23,
-		Title:       "Talheres",
-		Description: "Faqueiro Jogo De Talheres Conjunto Inox Escovado 24 Peças Hamilton Beach Aspen",
-		Picture:     "https://m.media-amazon.com/images/I/41YglxqCpDL._AC_SL1000_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B0FK5XTLT8",
-		Available:   true,
-	},
-	{
-		ID:          24,
-		Title:       "Kit Churrasco",
-		Description: "Kit Churrasco com Tabua, Kit Churrasco, Kit Churrasco Inox, Kit Churrasco Tradicional Inox 6 Peças, Kit Tábua para Churrasco, Kit de Churrasco 6 Peças com Tábua de Bambu",
-		Picture:     "https://m.media-amazon.com/images/I/61y3C7GbLlL._AC_SL1000_.jpg",
-		Link:        "https://www.amazon.com.br/dp/B0DQLBWH8R",
-		Available:   true,
-	},
+func loadWeddingPhotosFromJSON(path string) ([]string, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var photos []string
+	err = json.Unmarshal(data, &photos)
+	return photos, err
 }
 
-var weddingPhotos = WeddingPhotos{
-	Pictures: []string{
-		"https://m.media-amazon.com/images/I/61qQCckVsqL._AC_SL1500_.jpg",
-		"https://m.media-amazon.com/images/I/61qQCckVsqL._AC_SL1500_.jpg",
-		"https://m.media-amazon.com/images/I/61qQCckVsqL._AC_SL1500_.jpg",
-		"https://m.media-amazon.com/images/I/61qQCckVsqL._AC_SL1500_.jpg",
-		"https://m.media-amazon.com/images/I/61qQCckVsqL._AC_SL1500_.jpg",
-	},
+func loadWeddingListFromJSON(path string) ([]Wedding, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var list []Wedding
+	err = json.Unmarshal(data, &list)
+	return list, err
 }
 
 func main() {
 	godotenv.Load()
+
+	list, err := loadWeddingListFromJSON("wedding_list.json")
+	if err != nil {
+		fmt.Println("Erro ao carregar wedding_list.json:", err)
+		os.Exit(1)
+	}
+	weddingList := list
+
+	photos, err := loadWeddingPhotosFromJSON("wedding_photos.json")
+	if err != nil {
+		fmt.Println("Erro ao carregar wedding_photos.json:", err)
+		os.Exit(1)
+	}
+	weddingPhotos := WeddingPhotos{Pictures: photos}
 
 	fs := http.FileServer(http.Dir(os.Getenv("PATH_REACT")))
 	http.Handle("/", fs)
