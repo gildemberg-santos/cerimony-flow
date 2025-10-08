a2d3e2f79614181e8dee8154e1a2edcc2a9b33cc
Gildemberg Santos <gildemeber.santos@gmail.com>
2025-10-08 09:41:05 -0300

    filtrar itens disponíveis na lista de casamento e remover exibição de status de disponibilidade

--- frontend/src/components/WeddingList/index.jsx
+++ frontend/src/components/WeddingList/index.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Card, Col, Row } from 'react-bootstrap';
 import ButtonCustom from '../ButtonCustom';
 import SubTitleCustom from '../SubTitleCustom';
@@ -8,7 +7,7 @@ function WeddingList({ title, weddingList, handleShowModal }) {
     <div className="mb-4 p-4">
       <SubTitleCustom className="text-primary">{title}</SubTitleCustom>
       <Row className="justify-content-center">
-        {weddingList.map((item, index) => (
+        {weddingList.filter(item => item.available).map((item, index) => (
           <Col key={index} xs={12} sm={6} md={4} lg={3} className="mb-4">
             <Card className="shadow-sm border-0 p-4">
               <div className="d-flex justify-content-center align-items-center overflow-hidden p-4" style={{ width: '200px', height: '200px', margin: '0 auto' }}>
@@ -23,9 +22,6 @@ function WeddingList({ title, weddingList, handleShowModal }) {
               <Card.Body className="text-center">
                 <Card.Title className="text-olive">{item.title}</Card.Title>
                 <ButtonCustom onClick={() => handleShowModal(item)} className="btn btn-primary">Detalhes</ButtonCustom>
-                <p className={`mt-2 ${item.available ? 'text-success' : 'text-danger'} small`}>
-                  {item.available ? "Disponível" : "Indisponível"}
-                </p>
               </Card.Body>
             </Card>
           </Col>
