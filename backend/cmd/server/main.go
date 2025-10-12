5ccfad49d8237d9f03194c8378e8e4e6fde8ef89
Gildemberg Santos <gildemeber.santos@gmail.com>
2025-10-12 11:56:49 -0300

    feat: refactor main.go to use router for handling routes

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,64 +1,18 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
-	"os"
 
-	"github.com/gildemberg-santos/cerimony-flow/internal/settings"
-	"github.com/gildemberg-santos/cerimony-flow/internal/wedding_list"
-	"github.com/gildemberg-santos/cerimony-flow/internal/wedding_photos"
-	"github.com/joho/godotenv"
+	"github.com/gildemberg-santos/cerimony-flow/internal/router"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Default().Println("Erro ao carregar .env file", err)
-	}
+	mux := http.NewServeMux()
+	router.SetupRoutes(mux)
 
-	fs := http.FileServer(http.Dir(os.Getenv("PATH_REACT")))
-	http.Handle("/", fs)
-
-	http.HandleFunc("/settings", func(w http.ResponseWriter, r *http.Request) {
-		log.Default().Println("Requisição para /settings")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.WriteHeader(http.StatusOK)
-
-		resp, _ := json.Marshal(settings.NewSettings())
-		_, err := w.Write(resp)
-		if err != nil {
-			return
-		}
-	})
-
-	http.HandleFunc("/wedding-list", func(w http.ResponseWriter, r *http.Request) {
-		log.Default().Println("Requisição para /wedding-list")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.WriteHeader(http.StatusOK)
-
-		resp, _ := json.Marshal(wedding_list.NewWeddingList())
-		_, err := w.Write(resp)
-		if err != nil {
-			return
-		}
-	})
-
-	http.HandleFunc("/wedding-photos", func(w http.ResponseWriter, r *http.Request) {
-		log.Default().Println("Requisição para /wedding-photos")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.WriteHeader(http.StatusOK)
-
-		resp, _ := json.Marshal(wedding_photos.NewWeddingPhotos())
-		_, err = w.Write(resp)
-		if err != nil {
-			return
-		}
-	})
-
-	log.Default().Println("Server is running on port 8080 ...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Default().Println("Server is running on http://localhost:8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Default().Println("Erro ao carregar servidor.", err)
 	}
 }
